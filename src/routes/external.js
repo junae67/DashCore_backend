318fb0734edfca3b77e314f718607af28f714894
junae67 <escobarjuanp67@gmail.com>
2025-06-14 20:47:54 -0500

    se agrega a la ruta el middleware

--- src/routes/external.js
+++ src/routes/external.js
@@ -2,7 +2,12 @@
 const express = require('express');
 const router = express.Router();
 const externalFinanceController = require('../controllers/externalFinance');
+const identifyCompany = require('../middlewares/identifyCompany');
 
-router.post('/external/receive-finance-data', externalFinanceController.receiveFinanceData);
+router.post(
+  '/external/receive-finance-data',
+  identifyCompany,
+  externalFinanceController.receiveFinanceData
+);
 
 module.exports = router;
