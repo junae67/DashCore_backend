1588476640c9317377566c4f680cab675d4b20d5
junae67 <escobarjuanp67@gmail.com>
2025-11-18 12:40:11 -0500

    fix folder case issue

--- src/routes/erpConfig.routes.js
+++ src/routes/erpConfig.routes.js
@@ -47,7 +47,7 @@
 const express = require('express');
 const router = express.Router();
 const erpConfigController = require('../controllers/erpConfig.controller');
-const { authenticateToken } = require('../middlewares/auth');
+const { authenticateToken } = require('../middlewares/authMiddleware');
 const { identifyCompany } = require('../middlewares/identifyCompany');
 
 // Middleware: Autenticación y identificación de empresa
